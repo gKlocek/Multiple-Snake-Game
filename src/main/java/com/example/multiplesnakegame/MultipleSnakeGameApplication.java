ab01575dd826d8e097bb2447656eac3513c9ec3b
gKlocek <grzesiekklocek@gmail.com>
2022-12-22 17:51:53 +0100

    Initial commit

---
+++ src/main/java/com/example/multiplesnakegame/MultipleSnakeGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiplesnakegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleSnakeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleSnakeGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multiplesnakegame/MultipleSnakeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiplesnakegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleSnakeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
